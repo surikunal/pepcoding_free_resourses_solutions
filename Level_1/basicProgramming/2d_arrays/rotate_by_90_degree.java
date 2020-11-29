d4912ad41229663df70b6fada34f481bdebbcf4f
surikunal <suri.kunal.222@gmail.com>
2020-11-29 11:14:30 +0530

    update

---
+++ Level_1/basicProgramming/2d_arrays/rotate_by_90_degree.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+
+        int[][] ans = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][i] = arr[i][j];
+            }
+        }
+
+        for (int j = 0; j < n / 2; j++) {
+            for (int i = 0; i < n; i++) {
+                int temp = ans[i][j];
+                ans[i][j] = ans[i][n - j - 1];
+                ans[i][n - j - 1] = temp;
+            }
+        }
+        
+        display(ans);
+    }
+
+    public static void display(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
