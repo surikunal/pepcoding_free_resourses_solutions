a172121590a52652eb211e94e0007f2589732b8b
surikunal <suri.kunal.222@gmail.com>
2020-12-27 19:04:13 +0530

    basic

---
+++ Level_1/basicProgramming/getting_started/inverse_number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class inverse_number {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int ans = 0;
+        int op = 1;
+        while (n != 0) {
+            int od = n % 10;
+            int id = op;
+            int ip = od;
+
+            ans += id * (int) Math.pow(10, ip - 1);
+
+            n = n / 10;
+            op++;
+        }
+        System.out.println(ans);
+    }
+}
